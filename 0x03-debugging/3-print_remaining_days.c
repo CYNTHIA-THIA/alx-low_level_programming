3e93dbaafb26c5ebd8b66f659c162e58f930d852
CYNTHIA-THIA <ohabuirocynthia@yahoo.com>
2023-04-28 19:29:15 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * print_remaining_days - convert a date to the day of year and determines how many 
- * days are left in the year, taking leap year into consideration
- * @month: month in number format
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
 * @day: day of month
 * @year: year
 * Return: day of year
