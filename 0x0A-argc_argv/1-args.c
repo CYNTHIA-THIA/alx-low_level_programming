7ce7b1fdd32c3214ae16fa875567018cf0b2f2d9
CYNTHIA-THIA <ohabuirocynthia@yahoo.com>
2023-05-17 15:55:28 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print all the arguments it recieve
+ * @argc: has the length of the arguments
+ * @argv: has the arguments
+ * Return: 0 to indicate a good working of the program
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+	return (0);
+}
