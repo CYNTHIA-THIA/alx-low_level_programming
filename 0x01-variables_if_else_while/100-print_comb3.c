df0c8cd0b143aaf53a50688ff391890b14ba6732
CYNTHIA-THIA <ohabuirocynthia@yahoo.com>
2023-05-25 18:25:09 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
-
 /**
- * main - program that prints numbers from 0 to 99.
- * Return: 0
+ *main-program entry point.
+ *Return:0 success, non zero fail
  */
 int main(void)
 {
-int c = 0;
-while (c <= 99)
-{
-putchar(c / 10 + '0');
-putchar(c % 10 + '0');
-if (c != 99)
-{
-putchar(',');
-putchar(' ');
+	int i, k = '0', j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+			}
+			if (i == j)
+			{
+				continue;
+			}
+			if (i == '8' && j == '9')
+			{
+				break;
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		k++;
+	}
+	putchar('\n');
+	return (0);
 }
-c++;
-}
-putchar('\n');
-return (0);
-}
+
